bbff75cf06d93d927b0becd1a3d02d412d5304f7
Ruperto A Martinez <ponce11@email.unc.edu>
2023-11-29 11:57:31 -0500

    Universal navbar using layout.tsx across all pages.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import "./globals.css";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Ruperto's Portfolio",
@@ -9,6 +10,12 @@ export const metadata: Metadata = {
   },
 };
 
+const navigation = [
+  { name: "Skills", href: "/skills" },
+  { name: "Projects", href: "/projects" },
+  { name: "Resume", href: "/resume" },
+];
+
 export default function RootLayout({
   children,
 }: {
@@ -16,7 +23,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <nav className="absolute top-10 md:absolute md:top-2 md:right-10 md:mt-6 md:mr-6">
+          <ul className="flex items-left justify-left gap-4">
+            {navigation.map((item) => (
+              <Link
+                key={item.href}
+                href={item.href}
+                className="text-base text-neutral-400 hover:text-neutral-200"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </ul>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
