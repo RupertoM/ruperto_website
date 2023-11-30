b3e4c41f134b6620433edc1a6fdbab06a770ecf2
Ruperto A Martinez <ponce11@email.unc.edu>
2023-11-30 11:52:19 -0500

    Add backarrows and change backgrounds of projects and resume page.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import Link from "next/link";
+import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 
 export default function Projects() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-bl from-slate-500/20 via-slate-500/25 to-slate-500/20">
-      <h1>Projects Page.</h1>
+    <div className="w-screen h-screen overflow-hidden bg-gradient-to-br from-slate-500/10 via-slate-200/25 to-slate-500/10">
+      <Link href="/">
+        <KeyboardBackspaceIcon
+          style={{
+            fontSize: 40,
+            color: "white",
+            marginLeft: "1.5rem",
+            marginTop: "1.25rem",
+          }}
+        />
+      </Link>
+      <div className="flex flex-col items-start justify-start"></div>
     </div>
   );
 }
