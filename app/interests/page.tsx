b34182e9cb1ba341ac509a0516857afd7881c31c
Ruperto A Martinez <ponce11@email.unc.edu>
2023-12-13 02:27:54 -0500

    Complete volleyball card with slideshow button.

--- app/interests/page.tsx
+++ app/interests/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { VerseOfTheDay } from "../components/getVerse";
+import VolleyballSlideshow from "../components/vballSlideshow";
+import { Button } from "@mui/material";
 
 <link
   rel="stylesheet"
@@ -41,12 +43,36 @@ export default function Interests() {
                 alt=""
               />
             </div>
-            <div className="absolute inset-0 h-full w-full rounded-xl bg-black/80 px-12 text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]">
+            <div className="absolute inset-0 h-full w-full rounded-xl bg-black/80 px-6 text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]">
               <div className="flex min-h-full flex-col items-center justify-center">
-                <h1 className="text-2xl font-bold mb-4">Verse of the Day</h1>
-                <p className="text-base">
-                  Lorem ipsum dolor sit amet consectetur adipisicing.
-                </p>
+                <h1 className="text-md md:text-xl font-bold mb-0 md:mb-2">
+                  Indoor Volleyball
+                </h1>
+                <ul className="text-small md:text-base list-disc">
+                  <li>UNC Men's Outside Hitter</li>
+                  <li>NCVF National Tournaments</li>
+                  <li>ECVA Regional Tournaments</li>
+                </ul>
+
+                <h1 className="text-md md:text-xl font-bold mb-0 md:mb-2 mt-4 md:mt-8">
+                  Beach Volleyball
+                </h1>
+                <ul className="text-small md:text-base list-disc">
+                  <li>UNC Beach Volleyball</li>
+                  <li>AVP America Tournaments</li>
+                  <li>Open Level Matches</li>
+                </ul>
+
+                <Button
+                  variant="outlined"
+                  sx={{
+                    color: "white",
+                    borderColor: "white",
+                  }}
+                  className="hidden md:block mt-6 transition duration-300 ease-in-out hover:text-black hover:bg-white"
+                >
+                  Photo Gallery
+                </Button>
               </div>
             </div>
           </div>
