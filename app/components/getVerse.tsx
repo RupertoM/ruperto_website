3c77687f5be8bda0b8f1e5e847727c21bed1175a
Ruperto A Martinez <ponce11@email.unc.edu>
2024-01-09 15:35:40 -0500

    Add a generate new verse button to interests page.

--- app/components/getVerse.tsx
+++ app/components/getVerse.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
+import { Button } from "@mui/material";
 
 export const VerseOfTheDay: React.FC = () => {
   const [verseData, setVerseData] = useState({
@@ -8,47 +9,74 @@ export const VerseOfTheDay: React.FC = () => {
     reference: "",
   });
 
-  useEffect(() => {
-    const fetchVerseOfTheDay = async () => {
-      const options = {
-        method: "GET",
-        headers: { accept: "application/json" },
-      };
-
-      try {
-        const response = await fetch(
-          //Change the end of the url to (/get?format=json&order=daily) for verse of the day
-          "https://beta.ourmanna.com/api/v1/get?format=json&order=random",
-          options
-        );
-        const data = await response.json();
-
-        const verseDetails = data?.verse?.details;
-
-        setVerseData({
-          text:
-            verseDetails?.text ||
-            "Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight.",
-          reference: verseDetails?.reference || "Proverbs 3:5",
-        });
-      } catch (err) {
-        console.error(err);
-        setVerseData({
-          text: "Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight.",
-          reference: "Proverbs 3:5",
-        });
-      }
+  const fetchVerseOfTheDay = async () => {
+    const options = {
+      method: "GET",
+      headers: { accept: "application/json" },
     };
 
+    try {
+      const response = await fetch(
+        //Change the end of the url to (/get?format=json&order=daily) for verse of the day
+        "https://beta.ourmanna.com/api/v1/get?format=json&order=random",
+        options
+      );
+      const data = await response.json();
+
+      const verseDetails = data?.verse?.details;
+
+      setVerseData({
+        text:
+          verseDetails?.text ||
+          "Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight.",
+        reference: verseDetails?.reference || "Proverbs 3:5",
+      });
+    } catch (err) {
+      console.error(err);
+      setVerseData({
+        text: "Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight.",
+        reference: "Proverbs 3:5",
+      });
+    }
+  };
+
+  useEffect(() => {
     fetchVerseOfTheDay();
   }, []);
 
+  const handleGenerateNewVerse = () => {
+    fetchVerseOfTheDay();
+  };
+
   return (
     <div>
-      <h1 className="text-lg md:text-2xl font-bold mb-4">
+      <h1 className="text-md md:text-2xl font-bold mb-1 md:mb-4">
         {verseData.reference}
       </h1>
       <p className="text-sm md:text-base">{verseData.text}</p>
+
+      <Button
+        variant="outlined"
+        sx={{
+          marginTop: "0.5rem",
+          "@media (min-width: 768px)": {
+            marginTop: "1.5rem",
+          },
+          transition:
+            "color 300ms ease-in-out, background-color 300ms ease-in-out",
+          "&:hover": {
+            color: "black",
+            backgroundColor: "white",
+            borderColor: "white",
+            outline: "none",
+          },
+          color: "white",
+          borderColor: "white",
+        }}
+        onClick={handleGenerateNewVerse}
+      >
+        Generate New Verse
+      </Button>
     </div>
   );
 };
