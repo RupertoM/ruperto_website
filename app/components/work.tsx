cc7dcc70216f5501fe436e8a0f6f74c45be152c0
Ruperto A Martinez <ponce11@email.unc.edu>
2024-02-15 16:20:44 -0500

    Attempt to fix formatting issue on deployment.

--- app/components/work.tsx
+++ app/components/work.tsx
@@ -29,24 +29,24 @@ const WorkCards: React.FC = () => {
           {/* <Card className="w-2/5 bg-gradient-to-b from-neutral-800 from-5% via-neutral-600 to-neutral-800 to-90% rounded-xl"> */}
           <Card className="w-2/5 bg-gradient-to-tl from-neutral-400 via-white to-neutral-400 rounded-xl">
             <CardContent>
-              <Typography className="!font-bold text-xl special-font text-black text-center">
+              <Typography className="!font-bold !text-xl special-font text-black text-center">
                 Publicis Sapient
               </Typography>
-              <Typography className="text-black !font-bold text-base poppins-font text-center mt-2">
+              <Typography className="text-black !font-bold !text-base poppins-font text-center !mt-2">
                 Digital Business Transformation And Consulting
               </Typography>
               <div className="mt-3 flex">
-                <Typography className="text-black w-1/2 text-center !font-bold special-font text-sm">
+                <Typography className="text-black w-1/2 text-center !font-bold special-font !text-sm">
                   June 2023 - August 2023
                 </Typography>
-                <Typography className="text-black w-1/2 text-center !font-bold special-font text-sm">
+                <Typography className="text-black w-1/2 text-center !font-bold special-font !text-sm">
                   Atlanta, Georgia
                 </Typography>
               </div>
-              <Typography className="text-black text-center mt-2 !font-bold special-font text-sm">
+              <Typography className="text-black text-center !mt-2 !font-bold special-font !text-sm">
                 Software Engineering Intern
               </Typography>
-              <Typography className="text-black text-justify mt-6 !font-bold special-font text-sm mr-2 ml-2">
+              <Typography className="text-black text-justify !mt-6 !font-bold special-font !text-sm !mr-2 !ml-2">
                 As a software engineering intern, I was responsible for
                 developing and maintaining both the front-end and back-end of
                 the client&rsquo;s website. Using technologies such as React,
@@ -63,24 +63,24 @@ const WorkCards: React.FC = () => {
           </Card>
           <Card className="w-2/5 bg-gradient-to-tl from-neutral-400 via-white to-neutral-400 rounded-xl">
             <CardContent>
-              <Typography className="!font-bold text-xl special-font text-black text-center">
+              <Typography className="!font-bold !text-xl special-font text-black text-center">
                 Empowr Volleyball
               </Typography>
-              <Typography className="text-black !font-bold text-base poppins-font text-center mt-2">
+              <Typography className="text-black !font-bold !text-base poppins-font text-center !mt-2">
                 Men&rsquo;s Youth Beach and Indoor Volleyball
               </Typography>
               <div className="mt-3 flex">
-                <Typography className="text-black w-1/2 text-center !font-bold special-font text-sm">
+                <Typography className="text-black w-1/2 text-center !font-bold special-font !text-sm">
                   November 2023 - Present
                 </Typography>
-                <Typography className="text-black w-1/2 text-center !font-bold special-font text-sm">
+                <Typography className="text-black w-1/2 text-center !font-bold special-font !text-sm">
                   Cary, North Carolina
                 </Typography>
               </div>
-              <Typography className="text-black text-center mt-2 !font-bold special-font text-sm">
+              <Typography className="text-black text-center !mt-2 !font-bold special-font !text-sm">
                 18s South Head Coach
               </Typography>
-              <Typography className="text-black text-justify mt-6 !font-bold special-font text-sm mr-2 ml-2">
+              <Typography className="text-black text-justify !mt-6 !font-bold special-font !text-sm mr-2 ml-2">
                 As a head coach, I am responsible for the development of the
                 young players. I help them individually and as a collective.
                 Throughout the season, I analyze what the team as a whole
